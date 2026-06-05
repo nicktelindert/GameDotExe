c68a86ddde3abc9c8b830dd105f3610412fd5fcd
Nick <nicktelindert@protonmail.com>
2026-06-05 19:51:22 +0200

    Update test

--- test_presenters.py
+++ test_presenters.py
@@ -33,8 +33,10 @@ class TestMainPresenter(unittest.TestCase):
     def test_delete_game_confirmed(self, mock_exists, mock_rmtree):
         # Test of een game correct wordt verwijderd na bevestiging
         mock_exists.return_value = True
-        self.mock_view.confirm_deletion.return_value = True
         self.mock_config.get_path.return_value = "/games"
+        # We moeten db_path instellen omdat delete_game dit nu gebruikt voor ISO opruimen
+        self.mock_db.db_path = "/config/games.db"
+
         game_info = MagicMock()
         game_info.name = "TestGame"
         game_info.folder_name = "test_game"
@@ -44,7 +46,11 @@ class TestMainPresenter(unittest.TestCase):
         
         # Assert
         self.mock_db.delete_game.assert_called_with("test_game")
-        mock_rmtree.assert_called_with(os.path.join("/games", "test_game"))
+
+        # Er worden nu twee mappen verwijderd: de game map en de ISO map
+        mock_rmtree.assert_any_call(os.path.join("/games", "test_game"))
+        mock_rmtree.assert_any_call(os.path.join("/config", "ISOS", "test_game"))
+
         self.mock_view.load_games.assert_called()
 
 if __name__ == '__main__':
